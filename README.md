f38cc08437e9ba05951e8357263f33f87c136f92
Enrique <enrique.delgado@analyticalways.com>
2021-07-24 21:16:11 +0200

    Version 0.2 **Changelist**     - Removed glue on favor of stringi due to performance.     - Added further configuration for multiple outputs.     - Changed "warn" method to "warning".     - Changed "deb" method to "debug".     - Added class constructor for better initializing.     - Moved all outputing to a single method. No way for now to       make it private. **Known issues**     - Windows based executions may display a weird behavior when saving       formatted messages to a text file. **Pending additions**     - Testing.     - Better self-documentation.     - Database support.     - Quality checks on initialization.

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Declare an instance of the Logger class and you are free to go!
 
 ```{r}
 source('logger.R')
-logger <- Logger(log_level = 'INFO', filename = 'default', topic = 'main')
-logger$deb("This is a debug message. It won't show as INFO level is below DEBUG.")
+logger <- Logger(log_level = 'INFO', topic = 'main')
+logger$debug("This is a debug message. It won't show as INFO level is below DEBUG.")
 logger$info("This is an info message.")
-logger$warn("This is a warning message.")
+logger$warning("This is a warning message.")
 logger$error("This is an error message.")
 logger$fatal("This is a fatal message.")
 logger$traces("This is a traces message. It will go to a different file and will not show!")
@@ -27,3 +27,20 @@ Each level includes all levels below, with "OFF" meaning that no message will be
 I recommend to initialise the Logger instance on .GlobalEnv (i.e. `assign('logger', Logger(log_level = 'INFO', filename = 'default'), envir = .GlobalEnv)` for better availability.
 
 An additional parameter is set (topic) to include further information about the execution circumstances, to be added as the name of a file or subprocess. 
+
+## Installation
+You will need Stringi v. 1.6.2.
+
+Just source the script and follow the example in the previous section.
+
+## Configuration
+If you want your logs to be output to a file, you will need to set the parameter filename to a location of your choice (.e.g. 'logs/execution.log'). There is a flag that can be also configured to override each type of outputs: `console_output`, for direct output to stdio; `file_output`, to write into a file (A+ mode), `database_output`, to write directly to a database (unsupported).
+
+## To Do
+ - Better self-documentation.
+ - Testing.
+ - Database support.
+ - Checks.
+ 
+## Known issues
+ - Windows may show weird results in text-based logging because of the behaviour of `cat()` with Windows when printing formatted strings.
--- logger.R
+++ logger.R
@@ -4,71 +4,148 @@
 ## Coded by Enrique Delgado        ##
 #####################################
 
-library(glue)
+library(stringi)
 
-Logger = setRefClass("Logger",
-            fields = list(log_level="character",
-                          filename="character",
-                          topic="character"),
-            methods = list(
-              get_level=function(){
-                levels = c("OFF","FATAL","ERROR",
-                           "WARN","INFO","DEBUG")
-                return(which(levels %in% .self$log_level))
-              },
-              deb = function(msg){
-                ts_label = paste0("[", Sys.time(), "][", topic ,"][DEBUG] ")
-                if(get_level() >= 6){
-                  cat(paste0("\u001b[95m\u001b[1m",ts_label,msg,"\u001b[39m\u001b[22m\n"))
-                  if(length(filename) != 0){
-                    cat(paste0(ts_label,msg,"\n"), file = glue("{filename}.log"),
-                        append = TRUE) 
-                  }
-                }
-              },
-              info = function(msg){
-                ts_label = paste0("[", Sys.time(), "][", topic ,"][INFO] ")
-                if(get_level() >= 5){
-                  cat(paste0("\u001b[92m\u001b[1m",ts_label,msg,"\u001b[39m\u001b[22m\n"))
-                  if(length(filename) != 0){
-                    cat(paste0(ts_label,msg,"\n"), file = glue("{filename}.log"),
-                        append = TRUE) 
-                  }
-                }
-              },
-              warn = function(msg){
-                ts_label = paste0("[", Sys.time(), "][", topic ,"][WARN] ")
-                if(get_level() >= 4){
-                  cat(paste0("\u001b[93m\u001b[1m",ts_label,msg,"\u001b[39m\u001b[22m\n"))
-                  if(length(filename) != 0){
-                    cat(paste0(ts_label,msg,"\n"), file = glue("{filename}.log"),
-                        append = TRUE) 
-                  }
-                }
-              },
-              error = function(msg){
-                ts_label = paste0("[", Sys.time(), "][", topic ,"][ERROR] ")
-                if(get_level() >= 3){
-                  cat(paste0("\u001b[91m\u001b[1m",ts_label,msg,"\u001b[39m\u001b[22m\n"))
-                  if(length(filename) != 0){
-                    cat(paste0(ts_label,msg,"\n"), file = glue("{filename}.log"),
-                        append = TRUE) 
-                  }
-                }
-              },
-              fatal = function(msg){
-                ts_label = paste0("[", Sys.time(), "][", topic ,"][FATAL] ")
-                if(get_level() >= 2){
-                  cat(paste0("\u001b[31m\u001b[1m\u001b[40m",ts_label,msg,"\u001b[39m\u001b[22m\u001b[49m\n"))
-                  if(length(filename) != 0){
-                    cat(paste0(ts_label,msg,"\n"), file = glue("{filename}.log"), 
-                        append = TRUE) 
-                  }
-                }
-              },
-              traces = function(msg){
-                ts_label = paste0("[", Sys.time(), "][", topic ,"][FATAL] ")
-                cat(paste0(ts_label,msg,"\n"), file = glue("{filename}_TRACE.log"),
-                    append = TRUE)
-              }
-            ))
+Logger = setRefClass(
+  "Logger",
+  fields = list(
+    log_level = "character",
+    filename = "character",
+    topic = "character",
+    console_output = "logical",
+    file_output = "logical",
+    database_output = "logical"
+  ),
+  methods = list(
+    initialize = function(...) {
+      callSuper(...)
+      .self$log_level <-
+        ifelse(length(as.character(log_level)) == 0,
+               "OFF",
+               as.character(log_level))
+      .self$topic <-
+        ifelse(length(as.character(topic)) == 0,
+               "",
+               stri_c("[", as.character(topic), "]"))
+      .self$filename <-
+        ifelse(length(as.character(filename)) == 0,
+               "",
+               as.character(filename))
+      .self$console_output <-
+        ifelse(length(as.logical(console_output)) == 0,
+               TRUE,
+               as.logical(console_output))
+      .self$file_output <-
+        ifelse(
+          length(as.logical(file_output)) == 0,
+          ifelse(filename == "",
+                 FALSE,
+                 TRUE),
+          as.logical(file_output)
+        )
+      .self$database_output <-
+        ifelse(length(as.logical(database_output)) == 0,
+               FALSE,
+               as.logical(database_output))
+      .self
+    },
+    get_level = function() {
+      levels = c("OFF", "FATAL", "ERROR",
+                 "WARN", "INFO", "DEBUG")
+      return(which(levels %in% .self$log_level))
+    },
+    output = function(msg) {
+      if (console_output) {
+        cat(msg)
+      }
+      if (file_output) {
+        cat(msg, file = filename, append = T)
+      }
+      if (database_output) {
+      }
+    },
+    debug = function(msg) {
+      if (get_level() >= 6) {
+        msg <- stri_c(
+          "\u001b[95m\u001b[1m\u001b[1m[",
+          Sys.time(),
+          "]",
+          topic,
+          "[DEBUG] ",
+          msg,
+          "\u001b[39m\u001b[22m\u001b[49m\n"
+        )
+        output(msg)
+      }
+    },
+    info = function(msg) {
+      if (get_level() >= 5) {
+        msg <-
+          stri_c(
+            "\u001b[92m\u001b[1m\u001b[1m[",
+            Sys.time(),
+            "]",
+            topic ,
+            "[INFO] ",
+            msg,
+            "\u001b[39m\u001b[22m\u001b[49m\n"
+          )
+        output(msg)
+      }
+    },
+    warning = function(msg) {
+      if (get_level() >= 4) {
+        msg <-
+          stri_c(
+            "\u001b[93m\u001b[1m\u001b[1m[",
+            Sys.time(),
+            "]",
+            topic ,
+            "[WARNING] ",
+            msg,
+            "\u001b[39m\u001b[22m\u001b[49m\n"
+          )
+        output(msg)
+      }
+    },
+    error = function(msg) {
+      if (get_level() >= 3) {
+        msg <-
+          stri_c(
+            "\u001b[91m\u001b[1m\u001b[1m[",
+            Sys.time(),
+            "]",
+            topic ,
+            "[ERROR] ",
+            msg,
+            "\u001b[39m\u001b[22m\u001b[49m\n"
+          )
+        output(msg)
+      }
+    },
+    fatal = function(msg) {
+      if (get_level() >= 2) {
+        msg <-
+          stri_c(
+            "\u001b[31m\u001b[1m\u001b[40m[",
+            Sys.time(),
+            "]",
+            topic ,
+            "[FATAL] ",
+            msg,
+            "\u001b[39m\u001b[22m\u001b[49m\n"
+          )
+        output(msg)
+      }
+    },
+    traces = function(msg) {
+      ts_label = stri_c("[", Sys.time(), "]", topic , "[FATAL]\n")
+      cat(
+        stri_c(ts_label, msg, "\n"),
+        file = stri_c(filename, "_TRACE.log"),
+        append = TRUE
+      )
+    }
+    
+  )
+)
